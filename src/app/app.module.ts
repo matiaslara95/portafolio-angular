643579fe8ebd607c5b94e0ec7aa9126a4c0d383b
Matias Lara <matiaslara94@gmail.com>
2018-08-22 17:43:21 -0300

    Interfaz con info de pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
 import { AboutComponent } from './pages/about/about.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ItemComponent } from './pages/item/item.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ItemComponent } from './pages/item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor( private http: HttpClient ) {
+    console.log('Servicio de info pagina listo!');
+
+    // Leer archivoJSON
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (response: InfoPagina) => {
+      this.cargada = true;
+      this.info = response;
+      console.log(response);
+    });
+   }
+}

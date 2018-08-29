64215755762cdf89d8e84f7962f5eaba98a78b2d
Matias Lara <matiaslara94@gmail.com>
2018-08-29 12:13:18 -0300

    Cargando Funcionando

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
+import { timeout } from '../../../node_modules/@types/q';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductosService {
   cargando = true;
+  productos: Producto[] = [];
 
   constructor( private http: HttpClient) {
     this.cargarProductos();
@@ -15,8 +17,10 @@ export class ProductosService {
   private cargarProductos() {
     this.http.get('https://portafolio-angular-716bc.firebaseio.com/productos_idx.json')
       .subscribe( (response: Producto[]) => {
+
         console.log(response);
-        this.cargando = false;
+        this.productos = response;
+          this.cargando = false;
       });
   }
 }
